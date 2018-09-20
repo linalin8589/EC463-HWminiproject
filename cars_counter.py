986f1c99a4fe77be95d2dd830101e0d51e58c55e
Lin Wei <lina8589@signals47.bu.edu>
2018-09-20 00:56:53 -0400

    added comments to cars_counter.py

--- cars_counter.py
+++ cars_counter.py
@@ -6,24 +6,22 @@ import numpy as np
 import time
 import datetime
 
-foo = open("time.txt","w")
-startTime = time.time()
+foo = open("time.txt","w") 
 car_counter = 0
 
 #create VideoCapture object and read from video file
 cap = cv2.VideoCapture('cars_mass_pike_480.mp4')
-#create BackgroundSubtraction Object
 
 #use trained cars XML classifiers
 car_cascade = cv2.CascadeClassifier('cars.xml')
+
+startTime = time.time() #start counter to time
 
 #read until video is completed
 while True:
     #capture frame by frame
     ret, frame = cap.read()
 
-    #create foreground mask
-    
 
     #convert video into gray scale of each frames
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -31,7 +29,7 @@ while True:
     #detect cars in the video
     cars = car_cascade.detectMultiScale(gray, 1.1, 3)
 
-    #to draw arectangle in each cars 
+    #Draw a rectangle around each car 
     for (x,y,w,h) in cars:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)      
 
@@ -40,9 +38,10 @@ while True:
    
     #count the number of cars
     if len(cars) > car_counter:
-        currentTime = time.time() - startTime
-        car_counter = len(cars)
-        foo.write(str(car_counter))
+        currentTime = time.time() - startTime #take the time when number of cars increased
+        car_counter = len(cars) #updeta the number of cars counted
+        #write the results to a txt file The results can be ploted using gnuplot or excel
+        foo.write(str(car_counter)) 
         foo.write("  ")
         foo.write(str(currentTime))
         foo.write("\n")
